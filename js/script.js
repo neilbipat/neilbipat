3f56fa2506d94ab28d1ee4f080471a498231add1
Neil BipatE <neilbipat@gmail.com>
2019-10-27 13:50:26 -0400

    Working around with the modal

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 console.log('Hello');
+
+// let for the images over at html
+let imgOne = document.querySelector('#img1');
+
+// let for the main Modal and the Img port where the images will show in the dock
+let mainModal = document.querySelector('#picModal');
+let imgInModal = document.querySelector('#showImgs');
+
+// Event Listeners to grow picture
+imgOne.addEventListener('click', growPicture);
+
+// Click the pictures and it opens the modal and grows the pic
+function growPicture() {
+    mainModal.style.display = 'block';
+    imgInModal.src = this.src;
+}
+
+// Closes the modal by clicking anywhere off the picture
+window.onclick = function (e) {
+    if (e.target == picModal) {
+        picModal.style.display = "none";
+    }
+}
